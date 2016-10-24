1c3b3581b53e517225cfbf5982357d7eecae9e9b
yannis <yannis.lg@gmail.com>
2016-10-24 22:56:38 +0200

    battery charac

---
+++ charact_battery.js
@@ -0,0 +1,28 @@
+var util = require('util');
+var exec = require('child_process').exec;
+var bleno = require('bleno');
+
+var Descriptor = bleno.Descriptor;
+var BlenoCharacteristic = bleno.Characteristic;
+
+var BatteryLevelCharacteristic = function () {
+  BatteryLevelCharacteristic.super_.call(this, {
+    uuid: '2A19',
+    properties: ['read']
+  });
+};
+
+util.inherits(BatteryLevelCharacteristic, BlenoCharacteristic);
+
+BatteryLevelCharacteristic.prototype.onReadRequest = function (offset, callback) {
+  console.log('DhtCharacteristic - onReadRequest');
+
+  exec("battery-voltage",function (error, stdout, stderr) {
+    //Battery Voltage = 3460 mV
+    //Battery level = 5%
+    var percent = stdout.read.split('=')[3].split('%')[0];
+    callback(this.RESULT_SUCCESS, new Buffer([percent]));
+  });
+};
+
+module.exports = BatteryLevelCharacteristic;
--- test_ble.js
+++ test_ble.js
@@ -13,7 +13,7 @@ bleno.on('stateChange', function (state) {
   console.log('on -> stateChange: ' + state);
 
   if (state === 'poweredOn') {
-    bleno.startAdvertising('dht', ['180F']);
+    bleno.startAdvertising('dht', [myuuid]);
   } else {
     bleno.stopAdvertising();
   }
@@ -25,74 +25,10 @@ bleno.on('advertisingStart', function (error) {
   if (!error) {
     bleno.setServices([
       new BlenoPrimaryService({
-        uuid: '180F',
-        characteristics: [new DhtCharacteristic()]
+        uuid: myuuid,
+        characteristics: [new BatteryLevelCharacteristic()]
       })
     ]);
   }
 });
 
-var util = require('util');
-var Descriptor = bleno.Descriptor;
-var BlenoCharacteristic = bleno.Characteristic;
-var DhtCharacteristic = function () {
-  DhtCharacteristic.super_.call(this, {
-    uuid: '2A19',
-    properties: ['read']
-    /*descriptors: [
-      new Descriptor({
-      uuid: '2901',
-      value: 'Battery level in %'
-      }),
-      new Descriptor({
-        uuid: '2A6E',
-        value: "Temperature in C"
-      }),
-      new Descriptor({
-        uuid: '2A6F',
-        value: "Humidity in %"
-      })]*/
-  });
-
-  this._updateValueCallback = null;
-};
-
-util.inherits(DhtCharacteristic, BlenoCharacteristic);
-
-DhtCharacteristic.prototype.onReadRequest = function (offset, callback) {
-  console.log('DhtCharacteristic - onReadRequest: value = ' + this._value.toString('hex'));
-
-  var ret = dht.read(31);
-
-  if (ret.valid) {
-    console.log('T=' + ret.t + " H=" + ret.h);
-    callback(this.RESULT_SUCCESS, new Buffer([100, ret.t, ret.h]));
-  }
-  else {
-    console.log('error');
-    callback(this.RESULT_SUCCESS, new Buffer(['error']));
-  }
-};
-/*
-DhtCharacteristic.prototype.onWriteRequest = function(data, offset, withoutResponse, callback) {
-  this._value = data;
-  console.log('DhtCharacteristic - onWriteRequest: value = ' + this._value.toString('hex'));
-
-  if (this._updateValueCallback) {
-    console.log('DhtCharacteristic - onWriteRequest: notifying');
-    this._updateValueCallback(this._value);
-  }
-
-  callback(this.RESULT_SUCCESS);
-};
-
-DhtCharacteristic.prototype.onSubscribe = function(maxValueSize, updateValueCallback) {
-  console.log('DhtCharacteristic - onSubscribe');
-  this._updateValueCallback = updateValueCallback;
-};
-
-DhtCharacteristic.prototype.onUnsubscribe = function() {
-  console.log('DhtCharacteristic - onUnsubscribe');
-  this._updateValueCallback = null;
-};
-*/
