fabb374be9c2ae06ce7553449c3105155a2ad44b
yannis <yannis.lg@gmail.com>
2016-10-25 04:28:52 +0200

    dht

--- dht.js
+++ dht.js
@@ -1,5 +1,14 @@
 var dht = require('node-dht-edison');
 
-var last = dht.read(31);
+var _lastread=null;
+var _lasttime=new Date();
+
+var last = function() {
+    if((_lasttime-new Date())>2000 && _lastread!=null) {    
+        _lastread=dht.read(31);
+    }
+    
+    return _lastread;
+}
 
 module.exports = last;
